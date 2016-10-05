0a1c17ea23193b8c7d05964e7765597bd8cf23ac
verpokl <verpokl@gmail.com>
2016-10-05 12:46:10 -0500

    Update MultiStatisticsAbstract.java
    same as statistics abstract

--- MultiStatisticsAbstract.java
+++ MultiStatisticsAbstract.java
@@ -4,6 +4,7 @@
 
 /**
  * @author pvelesko
+ * @author Edgar M.
  *
  */
 public abstract class MultiStatisticsAbstract extends StatisticsAbstract {
@@ -21,4 +22,50 @@ public abstract class MultiStatisticsAbstract extends StatisticsAbstract {
      */
     protected abstract int itemCount();
     
+    /**
+     * Find the day with minimum wind speed
+     * @return DataDay representing a day with minimum wind speed
+     */
+    @Override
+    public abstract DataDay getWindSpeedMinDay();
+
+    
+    /**
+     * Find the day with maximum wind speed
+     * @return DataDay representing it
+     */
+    @Override
+    public abstract DataDay getWindSpeedMaxDay();
+
+    
+    /**
+     * Find the day with average wind speed
+     * @return sample representing it
+     */
+    @Override
+    public abstract Sample getWindSpeedAvgDay();
+    
+    /**
+     * Find the day with minimum wind speed
+     * @return DataDay representing a day with minimum solar radiation
+     */
+    @Override
+    public abstract DataDay getSolarRadiationMinDay();
+
+    
+    /**
+     * Find the day with maximum solar radiation
+     * @return DataDay representing it
+     */
+    @Override
+    public abstract DataDay getSolarRadiationMaxDay();
+
+    
+    /**
+     * Find the day with average solar radiation
+     * @return Sample representing it
+     */
+    @Override
+    public abstract Sample getSolarRadiationAvgDay();
+
 }
